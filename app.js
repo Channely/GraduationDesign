83c79ec82f6287ddd68f555c5b552e0a0afba674
Channely <qijie29896@gmail.com>
2014-05-19 22:27:34 +0800

    change server port && add tutorial folder into public dir

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var flash = require('connect-flash');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(flash());
